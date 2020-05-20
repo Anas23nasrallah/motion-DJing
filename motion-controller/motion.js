d2dac485027647c869d0b459b5c7e13d696480a3
ido gueron <debigueron@Debis-MacBook-Air.local>
2020-05-20 22:00:22 +0300

    nothing really

--- motion-controller/motion.js
+++ motion-controller/motion.js
@@ -1,12 +1,12 @@
 class Motion{
     constructor(){
-        $(window).on('motion', function(ev, data){
-            var spot = $(data.spot.el);
-            spot.addClass('active');
-            setTimeout(function(){
-                spot.removeClass('active');
-            }, 230);
-        })
+        // $(window).on('motion', function(ev, data){
+        //     var spot = $(data.spot.el);
+        //     spot.addClass('active');
+        //     setTimeout(function(){
+        //         spot.removeClass('active');
+        //     }, 230);
+        // })
     }
     addSpot(data){
         var spot = $(data.spot.el);
